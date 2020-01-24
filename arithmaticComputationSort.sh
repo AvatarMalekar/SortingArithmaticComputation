e7abb28522c86a9af542b7bdf52693cadc2d21a6
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 11:41:58 +0530

    ADDED:did calculation for c+a/b and variables added for result

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 echo "--------------------------------------WELLCOME TO ARITHMATIC COMPUTATON AND SORTING------------------------------------------"
-read -p "Enter First Number:" a
+read -p "Enter First Number:" a 
 read -p "Enter Second Number:" b
 read -p "Enter Third Number:" c
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=`echo "scale=2; $c+$a/$b" | bc`
+echo "Arithmatic Operation result for number1+number2*number3 is:" $result1
+echo "Arithmatic Operation result for number1*number2+number3 is:" $result2
+echo "Arithmatic Operation result for number3+number1/number2 is:" $result3
 
-echo "Arithmatic Operation result for number1+number2*number3 is:" $(($a+$b*$c))
-echo "Arithmatic Operation result for number1*number2+number3 is:" $(($a*$b+$c))
