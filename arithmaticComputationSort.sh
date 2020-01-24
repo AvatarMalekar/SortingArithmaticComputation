3b7826d866024dba1fdb0119cf9e496291db3082
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 10:32:57 +0530

    ADDED:read three integer from user

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -1,2 +1,6 @@
+#!/bin/bash -x
 echo "--------------------------------------WELLCOME TO ARITHMATIC COMPUTATON AND SORTING------------------------------------------"
+read -p "Enter First Number:" a
+read -p "Enter Second Number:" b
+read -p "Enter Third Number:" c
 
