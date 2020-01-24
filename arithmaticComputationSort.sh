45046c5919ae623cd09b1ebee230177507a955b4
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 15:45:48 +0530

    ADDED:sorted elements of Array in descending order

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -10,7 +10,6 @@ result2=`echo "scale=2; $a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
 result4=`echo "($a%$a)+$b" | bc`
 
-
 ResultDictionary[result1]=$result1
 ResultDictionary[result2]=$result2
 ResultDictionary[result3]=$result3
@@ -20,8 +19,24 @@ for (( i=1; i<=4; i++ ))
 do
 	Array[$i]=${ResultDictionary[result"$i"]}
 done
+
 echo ${!ResultDictionary[@]}
 echo ${ResultDictionary[@]}
 echo "Array Values:"
 echo ${Array[@]}
+arrayLength=${#Array[@]}
 
+for (( i=1; i<$arrayLength; i++ ))
+do
+	for (( j=$(($i+1)); j<=$arrayLength; j++ ))
+	do
+		if [ ${Array[$i]%%.*} -lt ${Array[$j]%%.*} ]
+		then
+			temp=${Array[$i]}
+			Array[$i]=${Array[$j]}
+			Array[$j]=$temp
+		fi
+	done
+done
+echo "After Sorting Array in Descending Order"
+echo ${Array[@]}
