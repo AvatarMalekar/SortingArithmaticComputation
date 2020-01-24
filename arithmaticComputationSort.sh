94808c66392074a53e6b8f01a91abc53a9a1faf3
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 14:09:58 +0530

    ADDED:read values from dictionary to array

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -10,11 +10,18 @@ result2=`echo "scale=2; $a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
 result4=`echo "($a%$a)+$b" | bc`
 
+
 ResultDictionary[result1]=$result1
 ResultDictionary[result2]=$result2
 ResultDictionary[result3]=$result3
 ResultDictionary[result4]=$result4
 
+for (( i=1; i<=4; i++ ))
+do
+	Array[$i]=${ResultDictionary[result"$i"]}
+done
 echo ${!ResultDictionary[@]}
 echo ${ResultDictionary[@]}
+echo "Array Values:"
+echo ${Array[@]}
 
