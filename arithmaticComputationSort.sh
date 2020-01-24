5c435d6c0ee24edd06f1d85d1580e97667704682
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-24 11:58:26 +0530

    ADDED:did calculation for a%b+c

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -6,7 +6,8 @@ read -p "Enter Third Number:" c
 result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=`echo "scale=2; $c+$a/$b" | bc`
+result4=$((($a%$b)+$c))
 echo "Arithmatic Operation result for number1+number2*number3 is:" $result1
 echo "Arithmatic Operation result for number1*number2+number3 is:" $result2
 echo "Arithmatic Operation result for number3+number1/number2 is:" $result3
-
+echo "Arithmatic Operation result for number1%number2+number3 is:" $result4
